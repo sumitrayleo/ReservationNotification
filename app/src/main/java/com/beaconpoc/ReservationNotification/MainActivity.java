1d778a1334c60192c511957099518b73966fc3f1
Chandan Chaudhuri <cchaudhuri@gmail.com>
2016-10-05 01:15:40 -0500

    making changes

--- app/src/main/java/com/beaconpoc/ReservationNotification/MainActivity.java
+++ app/src/main/java/com/beaconpoc/ReservationNotification/MainActivity.java
@@ -23,9 +23,8 @@ public class MainActivity extends AppCompatActivity {
         MyApplication app = (MyApplication) getApplication();
 
         if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
-            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
-            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
-            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
+            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met, verify pre-conditions at " +
+                    "http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
         } else if (!app.isBeaconNotificationsEnabled()) {
             Log.d(TAG, "Enabling beacon notifications");
             app.enableBeaconNotifications();
