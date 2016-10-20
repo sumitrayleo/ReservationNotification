d82a92a5a1882786fa0064d236abf7b84d35cd67
psantra <pijushsantra29@gmail.com>
2016-10-20 17:14:47 -0500

    Added Get retrieveDeviceInformation in ServiceUtil Class

--- app/src/main/java/com/beaconpoc/ReservationNotification/webservice/ServiceUtils.java
+++ app/src/main/java/com/beaconpoc/ReservationNotification/webservice/ServiceUtils.java
@@ -3,6 +3,7 @@ package com.beaconpoc.ReservationNotification.webservice;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
@@ -62,6 +63,46 @@ public class ServiceUtils {
     }
 
 
+    /**
+     * Get retrieveDeviceInformation Service
+     * @return String
+     */
+    public static String retrieveDeviceInformation() {
+
+        String responseData="";
+        boolean result = false;
+        HttpClient hc = new DefaultHttpClient();
+        HttpGet httpGet= new HttpGet("http://10.0.2.2:8080/booking-engine/api/booking/device/info?appName=EHI");
+
+        try {
+
+            httpGet.setHeader("Content-type", "application/json");
+            httpGet.setHeader("Booking-API-Key","1Q7VbsO93CPDMxTyzYp0ADnXG56gaRymAsp3vRiTcfw");
+            httpGet.setHeader("appPlatform","android");
+
+            HttpResponse resp = hc.execute(httpGet);
+
+            if (resp != null) {
+                if (resp.getStatusLine().getStatusCode() == 204)
+                    result = true;
+            }
+
+            System.out.println("Status line retrieveDeviceInformation:  " + resp.getStatusLine().getStatusCode());
+            responseData= readResponse(resp);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        return responseData;
+    }
+
+    /**
+     * Post Push Notification Service
+     * @return String
+     */
+
     public static String postPushNotificationData() {
 
         String responseData="";
@@ -99,7 +140,7 @@ public class ServiceUtils {
                     result = true;
             }
 
-            System.out.println("Status line:  " + resp.getStatusLine().getStatusCode());
+            System.out.println("Status line postPushNotificationData:  " + resp.getStatusLine().getStatusCode());
             responseData= readResponse(resp);
 
         } catch (Exception e) {
