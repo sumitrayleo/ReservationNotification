c3c2adb459e44424efa31d6c750d1982fd2a40c8
psantra <pijushsantra29@gmail.com>
2016-11-01 11:03:30 -0500

    Modified code for making service call from Activity class

--- app/src/main/java/com/beaconpoc/ReservationNotification/MainActivity.java
+++ app/src/main/java/com/beaconpoc/ReservationNotification/MainActivity.java
@@ -1,9 +1,9 @@
 package com.beaconpoc.ReservationNotification;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +13,13 @@ import android.widget.Toast;
 import com.beaconpoc.ReservationNotification.webservice.ServiceUtils;
 import com.estimote.sdk.SystemRequirementsChecker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
     Button pushNotification;
+    Button retrieveDeviceInformation;
     ProgressBar progressBar;
+    ProgressBar progressBar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,21 @@ public class MainActivity extends AppCompatActivity {
                 new ExecuteTask().execute();
             }
         });
+
+
+
+        retrieveDeviceInformation =(Button) findViewById(R.id.button1);
+        progressBar1=(ProgressBar) findViewById(R.id.progressBar1);
+        progressBar1.setVisibility(View.GONE);
+
+
+        retrieveDeviceInformation.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                progressBar1.setVisibility(View.VISIBLE);
+                new ExecuteTask().execute();
+            }
+        });
+
     }
 
 
@@ -44,13 +61,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            return ServiceUtils.postPushNotificationData();
+           // return ServiceUtils.postPushNotificationData();
+            return ServiceUtils.retrieveDeviceInformation();
         }
 
         @SuppressLint("ShowToast")
         @Override
         protected void onPostExecute(String result) {
-            progressBar.setVisibility(View.GONE);
+            progressBar1.setVisibility(View.GONE);
             Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             System.out.println("Webservice Response:::::"+result);
 
